758f4638706c0cde70312468b587b99ed784e527
john_farrell <john_farrell@symantec.com>
2019-12-30 21:58:59 +0000

    adding missing react files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component'
+
+import { SearchBox } from './components/search-box/search-box.component'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
+  render() {
+    // equivalent to doing individually const monsters = this.state.monsters
+    // creating constants so we dont change the base state (as to reset would mean a new get request
+    const { monsters, searchField } = this.state;
+
+    // filter local const array
+    // 
+    const filteredMonsters = monsters.filter(monsters => 
+      monsters.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+    return (
+      <div className="App">
+        <h1> Monsters Rolodex </h1>
+        <SearchBox 
+          placeholder='search monsters...' 
+          handleChange={this.handleChange}
+          />
+        <CardList monsters={filteredMonsters}/>
+      </div>
+    );
+  }
+
+  // React Component life cycle method we can override
+  componentDidMount(){
+    // returns a promise
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    // use setState for better react redrawing
+    // setState is asynchronous. to debug us callback param () =>  console.log(this.state)
+    .then(users => this.setState({ monsters: users }))
+  }
 }
 
 export default App;
