6f2e3e0cc978b6f729e9732246add2229c57bdb4
john_farrell <john_farrell@symantec.com>
2019-12-30 22:03:29 +0000

    adding gh pages configs

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -6,5 +6,6 @@ export const CardList = props => {
 return <div className='card-list'>{
         props.monsters.map(monster => (
         <Card key={monster.id} monster={monster}/>
-        ))}</div>;
+        ))}
+        </div>;
 };
